24fcf37423173c58bbfcc937183b976a70dbfe1e
CheeseFrog <au.yousef+dev@gmail.com>
2016-10-29 13:26:29 +1100

    Update CookGetSet.js

--- CookGetSet.js
+++ CookGetSet.js
@@ -1,12 +1,12 @@
 function cookget(n) {
-	var l = window.localStorage.getItem("q_" + n);
+	var l = window.localStorage.getItem("$_" + n);
 	if (l) return JSON.parse(l);
 	return null;
 }
 
 function cookset(n, v) {
-	if (v == null) window.localStorage.removeItem("q_" + n);
-	else window.localStorage.setItem("q_" + n, JSON.stringify(v));
+	if (v == null) window.localStorage.removeItem("$_" + n);
+	else window.localStorage.setItem("$_" + n, JSON.stringify(v));
 	return v;
 }
 if (typeof window.localStorage == "undefined") {
@@ -14,12 +14,12 @@ if (typeof window.localStorage == "undefined") {
 		var c = document.cookie.split("; ");
 		for (var i = 0; i < c.length; i++) {
 			var j = c[i].split("=");
-			if (j[0] == n) break;
+			if (j[0] == "$_" + n) break;
 		};
 		return JSON.parse(j[1] || 0) || null;
 	}
 	cookset = function(n, v) {
-		document.cookie = n + "=" + JSON.stringify(v) + "; expires=" + (new Date(Date.parse((v == null) ? "1970" : "3000"))).toUTCString() + ";";
+		document.cookie = "$_" + n + "=" + JSON.stringify(v) + "; expires=" + (new Date(Date.parse((v == null) ? "1970" : "3000"))).toUTCString() + ";";
 		return v;
 	}
 }
